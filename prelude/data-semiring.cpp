07caf6e3106deac4a78bdd7fde57bd43953f819b
Andy Arvanitis <andyarvanitis@gmail.com>
2018-08-13 23:15:04 -0700

    More prelude implementations

---
+++ prelude/data-semiring.cpp
@@ -0,0 +1,39 @@
+#include "purescript.h"
+
+// Tested with package v4.0.0
+
+FOREIGN_BEGIN( Data_Semiring )
+
+exports["intAdd"] = [](const boxed& x_) -> boxed {
+    const auto x = unbox<int>(x_);
+    return [=](const boxed& y_) -> boxed {
+        const auto y = unbox<int>(y_);
+        return x + y;
+    };
+};
+
+exports["intMul"] = [](const boxed& x_) -> boxed {
+    const auto x = unbox<int>(x_);
+    return [=](const boxed& y_) -> boxed {
+        const auto y = unbox<int>(y_);
+        return x * y;
+    };
+};
+
+exports["numAdd"] = [](const boxed& x_) -> boxed {
+    const auto x = unbox<double>(x_);
+    return [=](const boxed& y_) -> boxed {
+        const auto y = unbox<double>(y_);
+        return x + y;
+    };
+};
+
+exports["numMul"] = [](const boxed& x_) -> boxed {
+    const auto x = unbox<double>(x_);
+    return [=](const boxed& y_) -> boxed {
+        const auto y = unbox<double>(y_);
+        return x * y;
+    };
+};
+
+FOREIGN_END;
