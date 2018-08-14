d23e264e6e2d944802435d866ee658c6b4436fd6
Andy Arvanitis <andyarvanitis@gmail.com>
2018-08-13 20:06:44 -0700

    More prelude implementations

--- prelude/data-eq.cpp
+++ prelude/data-eq.cpp
@@ -10,4 +10,25 @@ exports["eqStringImpl"] = [](const boxed& s1) -> boxed {
     };
 };
 
+exports["eqArrayImpl"] = [](const boxed& f) -> boxed {
+    return [=](const boxed& xs_) -> boxed {
+        return [=](const boxed& ys_) -> boxed {
+            const auto& xs = unbox<array_t>(xs_);
+            const auto& ys = unbox<array_t>(ys_);
+            if (xs.size() != ys.size()) {
+                return false;
+            }
+            for (auto itx = xs.cbegin(), end = xs.cend(), ity = ys.cbegin();
+                 itx != end;
+                 itx++, ity++) {
+                const auto equal = f(*itx)(*ity);
+                if (!unbox<bool>(equal)) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    };
+};
+
 FOREIGN_END;
---
+++ prelude/data-heytingalgebra.cpp
@@ -0,0 +1,25 @@
+#include "purescript.h"
+
+// Tested with package v4.0.0
+
+FOREIGN_BEGIN( Data_HeytingAlgebra )
+
+exports["boolConj"] = [](const boxed& b1_) -> boxed {
+    const auto b1 = unbox<bool>(b1_);
+    return [=](const boxed& b2) -> boxed {
+        return b1 && unbox<bool>(b2);
+    };
+};
+
+exports["boolDisj"] = [](const boxed& b1_) -> boxed {
+    const auto b1 = unbox<bool>(b1_);
+    return [=](const boxed& b2) -> boxed {
+        return b1 || unbox<bool>(b2);
+    };
+};
+
+exports["boolNot"] = [](const boxed& b) -> boxed {
+    return !unbox<bool>(b);
+};
+
+FOREIGN_END;
