fa80108e87bd291b899d610798b1c63416254440
Andy Arvanitis <andyarvanitis@gmail.com>
2018-08-13 23:12:22 -0700

    More prelude implementations

---
+++ prelude/data-bounded.cpp
@@ -0,0 +1,17 @@
+#include <limits>
+#include "purescript.h"
+
+// Tested with package v4.0.0
+
+FOREIGN_BEGIN( Data_Bounded )
+
+exports["topInt"] = std::numeric_limits<int>::max();
+exports["bottomInt"] = std::numeric_limits<int>::min();
+
+exports["topChar"] = 0x10FFFF; // unicode limit
+exports["bottomChar"] = 0;
+
+exports["topNumber"] = std::numeric_limits<double>::max();
+exports["bottomNumber"] = std::numeric_limits<double>::min();
+
+FOREIGN_END;
---
+++ prelude/data-ring.cpp
@@ -0,0 +1,16 @@
+#include "purescript.h"
+
+// Tested with package v4.0.0
+
+FOREIGN_BEGIN( Data_Ring )
+
+exports["intSub"] = [](const boxed& x_) -> boxed {
+    const auto x = unbox<int>(x_);
+    return [=](const boxed& y_) -> boxed {
+        const auto y = unbox<int>(y_);
+        return x - y;
+    };
+};
+
+
+FOREIGN_END;
