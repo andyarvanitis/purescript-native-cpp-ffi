ab5172e913765642b0470907133eb80f2115330c
Andy Arvanitis <andyarvanitis@gmail.com>
2018-08-16 21:56:08 -0700

    Add unsafePartial and more Data.Ord impls

---
+++ partial/partial-unsafe.cpp
@@ -0,0 +1,11 @@
+#include "purescript.h"
+
+// Tested with package v2.0.0
+
+FOREIGN_BEGIN( Partial_Unsafe )
+
+exports["unsafePartial"] = [](const boxed& f) -> boxed {
+    return f(nullptr);
+};
+
+FOREIGN_END;
--- prelude/data-ord.cpp
+++ prelude/data-ord.cpp
@@ -2,20 +2,30 @@
 
 // Tested with package v4.?.?
 
-FOREIGN_BEGIN( Data_Ord )
-
-exports["ordIntImpl"] = [](const boxed& lt) -> boxed {
-    return [=](const boxed& eq) -> boxed {
-        return [=](const boxed& gt) -> boxed {
-            return [=](const boxed& x_) -> boxed {
-                const auto x = unbox<int>(x_);
-                return [=](const boxed& y_) -> boxed {
-                    const auto y = unbox<int>(y_);
-                    return x < y ? lt : x == y ? eq : gt;
+namespace Data_Ord {
+    using namespace purescript;
+    template <typename T>
+    static auto ordImpl(const boxed& lt) -> boxed {
+        return [=](const boxed& eq) -> boxed {
+            return [=](const boxed& gt) -> boxed {
+                return [=](const boxed& x_) -> boxed {
+                    const auto x = unbox<T>(x_);
+                    return [=](const boxed& y_) -> boxed {
+                        const auto y = unbox<T>(y_);
+                        return x < y ? lt : x == y ? eq : gt;
+                    };
                 };
             };
         };
-    };
-};
+    }
+}
+
+FOREIGN_BEGIN( Data_Ord )
+
+exports["ordBooleanImpl"] = ordImpl<bool>;
+exports["ordIntImpl"]     = ordImpl<int>;
+exports["ordNumberImpl"]  = ordImpl<double>;
+exports["ordStringImpl"]  = ordImpl<string>;
+exports["ordCharImpl"]    = ordImpl<string>;
 
 FOREIGN_END;
